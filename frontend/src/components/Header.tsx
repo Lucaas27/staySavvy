2a2972c3e2b7f2e1e1f18f364d312a2a91630fd0
Lucas Gomes <lucasggomees@gmail.com>
2024-01-08 22:41:50 +0000

    Install prettier plugin for Tailwindcss and format all frontend files

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="bg-blue-800 py-6">
-      <div className="container max-auto flex justify-between">
-        <span className="text-3xl text-white font-bold tracking-tight">
+      <div className="container mx-auto flex justify-between">
+        <span className="text-3xl font-bold tracking-tight text-white">
           <Link to="/">Stay Savvy</Link>
         </span>
         <span className="flex space-x-2">
           <Link
             to="/sign-in"
-            className="flex items-center text-blue-600 px-3 font-bold hover:bg-gray-100"
+            className="flex items-center px-3 font-bold text-blue-600 hover:bg-gray-100"
           >
             Sign In
           </Link>
--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 
 const Layout = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <Header />
     </div>
   );
