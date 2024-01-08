e6efa120bc19490feda2bd4c436f299d01b84758
Lucas Gomes <lucasggomees@gmail.com>
2024-01-08 22:27:19 +0000

    Setup initial react front-end with tailwindcss

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Layout from "./layouts/Layout";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout></Layout>}></Route>
+        <Route path="/search">{<>Search Page</>}</Route>
+        <Route path="*" element={<Navigate to="/" />}></Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
