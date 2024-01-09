eacda6ead621c977bc7c180fe5a46008add657a6
Lucas Gomes <lucasggomees@gmail.com>
2024-01-09 13:04:32 +0000

    Add footer component

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer className="bg-custom-main py-10">
+      <div className="section-container flex items-center justify-between">
+        <span className="text-3xl font-bold tracking-tight text-custom-content">
+          Stay Savvy
+        </span>
+        <span className="flex gap-4 font-bold text-white">
+          <small className="cursor-pointer">Privacy Policy</small>
+          <small className="cursor-pointer">Terms of Service</small>
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
