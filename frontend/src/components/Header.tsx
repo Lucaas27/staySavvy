a7e690797200cd29ee8fddd9fd221140c0adf0af
Lucas Gomes <lucasggomees@gmail.com>
2024-01-09 13:06:02 +0000

    Update header component to include hero

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
+import Hero from "./Hero";
 
 const Header = () => {
   return (
     <header className="bg-custom-main py-6">
-      <nav className="section-container flex justify-between">
+      <nav className="section-container flex justify-between space-x-2">
         <span className="text-3xl font-bold tracking-tight text-custom-content">
           <Link to="/">Stay Savvy</Link>
         </span>
-        <span className="flex space-x-2">
+        <span className="flex min-w-max space-x-2">
           <Link
             to="/sign-in"
             className="flex items-center rounded-md bg-white px-3 font-bold text-custom-secondary hover:bg-gray-100"
@@ -16,6 +17,7 @@ const Header = () => {
           </Link>
         </span>
       </nav>
+      <Hero />
     </header>
   );
 };
--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,12 +1,11 @@
 const Hero = () => {
   return (
-    <section className="bg-custom-main pb-16">
-      <div className="section-container flex flex-col gap-2">
+    <section className="section-container bg-custom-main py-16 ">
+      <div className="flex flex-col gap-2">
         <h1 className="text-5xl font-bold text-custom-content">
           Find your next stay
         </h1>
         <p className="text-2xl text-custom-content">
-          {" "}
           Search low prices on hotels for your dream vacation...
         </p>
       </div>
