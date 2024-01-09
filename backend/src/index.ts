53791d5f94e2a7ca67062ee2bcc9afa00ecf83b2
Lucas Gomes <lucasggomees@gmail.com>
2024-01-09 15:42:32 +0000

    Create user registration endpoint with jwt authentication

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,16 +2,23 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+import userRoutes from "./routes/users";
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => {
+    console.info("Connected to MongoDB");
+  })
+  .catch((e) => {
+    console.error("Error connecting to MongoDB:", e.message);
+  });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.json({ message: "Hello world!" });
-});
+app.use("/api/users", userRoutes);
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.listen(3000, () => console.info("Server is running on port 3000"));
